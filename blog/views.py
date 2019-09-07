54babf97b8f61d130c4f1249a8945b57b45fd285
Shubham Subedi <intabcsc@gmail.com>
2019-09-07 22:06:32 +0545

    pagination completed

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.contrib.auth.models import User
 from django.views.generic import ( 
     ListView, 
     DetailView, 
@@ -36,6 +37,18 @@ class PostListView( ListView):
     template_name = 'blog/home.html' #<app>/<model>_<viewtype>.html
     context_object_name = 'posts_context'
     ordering = ['-date_posted']
+    paginate_by = 2
+
+class UserPostListView( ListView):
+    model = Post
+    template_name = 'blog/user_post.html' 
+    context_object_name = 'posts_context'
+    paginate_by = 2
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
+
 
 class PostDetailView(DetailView):
     model = Post
